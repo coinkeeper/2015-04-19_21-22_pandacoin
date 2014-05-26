6562b1cdbcd2be2bf414eef8ae8885e30508ebbf
pandacoin-official <pandacoin@hush.com>
2014-05-26 18:17:14 +0200

    Throw out time pair.

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -48,7 +48,6 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, Object& entry)
 {
     entry.push_back(Pair("txid", tx.GetHash().GetHex()));
     entry.push_back(Pair("version", tx.nVersion));
-    entry.push_back(Pair("time", (int64_t)tx.nTime));
     entry.push_back(Pair("locktime", (int64_t)tx.nLockTime));
     Array vin;
     BOOST_FOREACH(const CTxIn& txin, tx.vin)
